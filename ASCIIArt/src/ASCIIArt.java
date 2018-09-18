b1834fd8dd02307905045b5ee268eb7424b2b6a5
skinnyfish <s-xiel@bsd405.org>
2018-09-18 08:05:01 -0700

    Finished the Chessboard

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,3 +1,4 @@
+
 /**
  * Unit 1 Project
  * 
@@ -13,5 +14,61 @@
  * @author Mr. Rasmussen 
  *
  */
+public class ASCIIArt {
+	public static void main(String[] args) {
+		drawTop();
+		for (int i = 8;i>0;i--) {
+			drawRow(i);
+		}
+		drawBottom();
+		caption();
+	}
+	public static void drawRow(int row) {
+		if (row%2==0) {
+			for(int i = 0;i<3;i++) {
+				drawLines(7,true);
+			}
+		}
+		else {
+			for(int i = 0;i<3;i++) {
+				drawLines(7,false);
+			}
+		}
+	}
+	public static void drawLines(int length, Boolean space_in_front) {
+		System.out.print("|");
+		if (space_in_front) {
+			for (int i = 0;i<length;i++) {
+				System.out.print(" ");
+			}
+		}
+		for (int i = 0;i<4;i++) {
+			for (int j = 0;j<length;j++) {
+				System.out.print("+");
+			}
+			if (i==3 && space_in_front) {
+				break;
+			}
+			for (int k = 0;k<length;k++) {
+				System.out.print(" ");
+			}
+		}
+		System.out.print("|\n");
+	}
+	public static void drawTop() {
+		System.out.println("|--------------------------------------------------------|");
+		System.out.println("|   A      B      C      D      E      F      G      H   |");
+		System.out.println("|--------------------------------------------------------|");
 
-// YOUR CODE GOES HERE :)
+	}
+	public static void drawBottom() {
+		System.out.println("|--------------------------------------------------------|");
+		System.out.println("|   A      B      C      D      E      F      G      H   |");
+		System.out.println("|--------------------------------------------------------|");
+	}
+	public static void caption() {
+		System.out.println("                         /|\\");
+		System.out.println("                          |");
+		System.out.println("                  This is a chessboard");
+	}
+}
